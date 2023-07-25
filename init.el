2057e3d2ea1daaa349e40343aafab711d293b4a1
A7R7 <Aaron__Lee_@outlook.com>
2023-07-25 14:59:26 +0800

    update readme

--- init.el
+++ init.el
@@ -218,13 +218,13 @@
 
 (setq-default line-spacing 0.12)
 
-(use-package emacs
-  :init 
-    (global-set-key (kbd "C-=")            'text-scale-increase)
-    (global-set-key (kbd "C--")            'text-scale-decrease)
-    (global-set-key (kbd "<C-wheel-up>")   'text-scale-increase)
-    (global-set-key (kbd "<C-wheel-down>") 'text-scale-decrease)
-)
+ (use-package emacs
+   :init 
+     (global-set-key (kbd "C-=")            'text-scale-increase)
+     (global-set-key (kbd "C--")            'text-scale-decrease)
+     (global-set-key (kbd "<C-wheel-up>")   'text-scale-increase)
+     (global-set-key (kbd "<C-wheel-down>") 'text-scale-decrease)
+ )
 
 (use-package all-the-icons
   :ensure t
@@ -279,37 +279,37 @@
 
 (use-package diminish)
 
-(use-package centaur-tabs
-  :hook
-    (emacs-startup . centaur-tabs-mode)
-  :init
-    (setq centaur-tabs-set-icons t
-	  centaur-tabs-set-modified-marker t
-	  centaur-tabs-modified-marker "M"
-	  centaur-tabs-cycle-scope 'tabs
-	  centaur-tabs-set-close-button nil
-	  centaur-tabs-enable-ido-completion nil)
-  :config
-    (centaur-tabs-mode t)
-    ;; (centaur-tabs-headline-match)
-    (centaur-tabs-group-by-projectile-project)
-)
+  (use-package centaur-tabs
+    :hook
+      (emacs-startup . centaur-tabs-mode)
+    :init
+      (setq centaur-tabs-set-icons t
+	    centaur-tabs-set-modified-marker t
+	    centaur-tabs-modified-marker "M"
+	    centaur-tabs-cycle-scope 'tabs
+	    centaur-tabs-set-close-button nil
+	    centaur-tabs-enable-ido-completion nil)
+    :config
+      (centaur-tabs-mode t)
+      ;; (centaur-tabs-headline-match)
+      (centaur-tabs-group-by-projectile-project)
+  )
 
-(use-package evil
-  :init
-    (setq evil-want-integration t) ;; t by default
-    (setq evil-want-keybinding nil)
-    (setq evil-vsplit-window-right t)
-    (setq evil-split-window-below t)
-    (setq evil-want-minibuffer t) ;; use evil in minibuffer!
-  :config
-    (evil-mode 1)
-      ;; Use visual line motions even outside of visual-line-mode buffers
-    (evil-global-set-key 'motion "j" 'evil-next-visual-line)
-    (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
-    (evil-set-initial-state 'messages-buffer-mode 'normal)
-    (evil-set-initial-state 'dashboard-mode 'normal)
-)
+  (use-package evil
+    :init
+      (setq evil-want-integration t) ;; t by default
+      (setq evil-want-keybinding nil)
+      (setq evil-vsplit-window-right t)
+      (setq evil-split-window-below t)
+      (setq evil-want-minibuffer t) ;; use evil in minibuffer!
+    :config
+      (evil-mode 1)
+        ;; Use visual line motions even outside of visual-line-mode buffers
+      (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+      (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+      (evil-set-initial-state 'messages-buffer-mode 'normal)
+      (evil-set-initial-state 'dashboard-mode 'normal)
+  )
 
 (use-package evil-collection
   ;; :demand t
@@ -325,47 +325,47 @@
   :config (setq ring-bell-function #'ignore)
 )
 
-;; Make ESC quit prompts
-(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+  ;; Make ESC quit prompts
+  (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
-(use-package general
-:after evil
-:config
-  ;; (general-evil-setup)
-  ;; set up 'SPC' as the global leader key
-  (general-create-definer bind-leader-to
-    :states '(normal insert visual emacs)
-    :keymaps 'override
-    :prefix "SPC" ;; set leader
-    :global-prefix "M-SPC") ;; access leader in insert mode
-  (bind-leader-to
-      "b"  '(:ignore t                          :wk "Buffer")
-      "bb" '(switch-to-buffer                   :wk "Switch buffer")
-      "bd" '(kill-this-buffer                   :wk "Delete buffer")
-      "bp" '(previous-buffer                    :wk "Prev Buffer ")
-      "bn" '(next-buffer                        :wk "Next Buffer ")
-      "br" '(revert-buffer                      :wk "Reload Buffer")
-      "["  '(previous-buffer                    :wk "Prev Buffer ")
-      "]"  '(next-buffer                        :wk "Next Buffer ")
+  (use-package general
+  :after evil
+  :config
+    ;; (general-evil-setup)
+    ;; set up 'SPC' as the global leader key
+    (general-create-definer bind-leader-to
+      :states '(normal insert visual emacs)
+      :keymaps 'override
+      :prefix "SPC" ;; set leader
+      :global-prefix "M-SPC") ;; access leader in insert mode
+    (bind-leader-to
+	"b"  '(:ignore t                          :wk "Buffer")
+	"bb" '(switch-to-buffer                   :wk "Switch buffer")
+	"bd" '(kill-this-buffer                   :wk "Delete buffer")
+	"bp" '(previous-buffer                    :wk "Prev Buffer ")
+	"bn" '(next-buffer                        :wk "Next Buffer ")
+	"br" '(revert-buffer                      :wk "Reload Buffer")
+	"["  '(previous-buffer                    :wk "Prev Buffer ")
+	"]"  '(next-buffer                        :wk "Next Buffer ")
 
-      "w"  '(:ignore t                          :wk "Window")
-      "wd" '(delete-window                      :wk "Delete window")
-      "wv" '(split-window-vertically            :wk "V Split 󰤼 ")
-      "wh" '(split-window-horizontally          :wk "H Split 󰤻 ")
-      "wh" '(evil-window-left                   :wk "window ")
-      "wj" '(evil-window-down                   :wk "window ")
-      "wk" '(evil-window-up                     :wk "window ")
-      "wl" '(evil-window-right                  :wk "window ")
+	"w"  '(:ignore t                          :wk "Window")
+	"wd" '(delete-window                      :wk "Delete window")
+	"wv" '(split-window-vertically            :wk "V Split 󰤼 ")
+	"wh" '(split-window-horizontally          :wk "H Split 󰤻 ")
+	"wh" '(evil-window-left                   :wk "window ")
+	"wj" '(evil-window-down                   :wk "window ")
+	"wk" '(evil-window-up                     :wk "window ")
+	"wl" '(evil-window-right                  :wk "window ")
 
-      "p"  '(:ignore t                          :wk "Package(Borg)")
-      "pa" '(borg-assimilate                    :wk "Assimilate")
-      "pc" '(borg-clone                         :wk "Clone")
-      "pr" '(borg-remove                        :wk "Remove")
+	"p"  '(:ignore t                          :wk "Package(Borg)")
+	"pa" '(borg-assimilate                    :wk "Assimilate")
+	"pc" '(borg-clone                         :wk "Clone")
+	"pr" '(borg-remove                        :wk "Remove")
 
-      "t"  '(:ignore t                          :wk "Toggle")
+	"t"  '(:ignore t                          :wk "Toggle")
 
+    )
   )
-)
 
 (use-package which-key
   :init
@@ -387,35 +387,35 @@
   (which-key-mode 1)
 )
 
-(use-package org
-  :config
-  (set-face-attribute 'org-level-1 nil :family "Cantarell" :height 1.5 :bold t)
-  (set-face-attribute 'org-level-2 nil :family "Cantarell" :height 1.25 :bold t)
-  (set-face-attribute 'org-level-3 nil :family "Cantarell" :height 1.1 :bold t)
-  (set-face-attribute 'org-level-4 nil :family "Cantarell" :height 1.05 :bold t)
-  (set-face-attribute 'org-level-5 nil :family "Cantarell" :height 1.05 :bold t)
-  (set-face-attribute 'org-level-6 nil :family "Cantarell" :height 1.05 :bold t)
-  (set-face-attribute 'org-document-title nil :family "Cantarell" :height 1.75 :bold t)
-  (setq org-adapt-indentation t)
-  (setq org-indent-indentation-per-level 1)
-)
-
-(use-package org-superstar
-:hook (org-mode . org-superstar-mode)
-:init
-  (setq
-    ;;org-superstar-headline-bullets-list '("✖" "✚" "◉" "○" "▶")
-    org-superstar-special-todo-items t
-    org-ellipsis "  "
+  (use-package org
+    :config
+    (set-face-attribute 'org-level-1 nil :family "Cantarell" :height 1.5 :bold t)
+    (set-face-attribute 'org-level-2 nil :family "Cantarell" :height 1.25 :bold t)
+    (set-face-attribute 'org-level-3 nil :family "Cantarell" :height 1.1 :bold t)
+    (set-face-attribute 'org-level-4 nil :family "Cantarell" :height 1.05 :bold t)
+    (set-face-attribute 'org-level-5 nil :family "Cantarell" :height 1.05 :bold t)
+    (set-face-attribute 'org-level-6 nil :family "Cantarell" :height 1.05 :bold t)
+    (set-face-attribute 'org-document-title nil :family "Cantarell" :height 1.75 :bold t)
+    (setq org-adapt-indentation t)
+    (setq org-indent-indentation-per-level 1)
   )
-)
 
-(use-package hl-todo
+  (use-package org-superstar
+  :hook (org-mode . org-superstar-mode)
   :init
-  (hl-todo-mode)
-)
+    (setq
+      ;;org-superstar-headline-bullets-list '("✖" "✚" "◉" "○" "▶")
+      org-superstar-special-todo-items t
+      org-ellipsis "  "
+    )
+  )
 
-(use-package org-fancy-priorities)
+  (use-package hl-todo
+    :init
+    (hl-todo-mode)
+  )
+
+  (use-package org-fancy-priorities)
 
 (use-package org-appear
   :hook (org-mode . org-appear-mode)
@@ -613,45 +613,45 @@
   (global-lsp-bridge-mode)
 )
 
-(use-package dirvish
-  :init
-  (dirvish-override-dired-mode)
-  :custom
-  (dirvish-quick-access-entries ; It's a custom option, `setq' won't work
-   '(("h" "~/"                          "Home")
-     ("d" "~/Downloads/"                "Downloads")
-     ("m" "/mnt/"                       "Drives")
-     ("t" "~/.local/share/Trash/files/" "TrashCan")))
-  :config
-  ;; (dirvish-peek-mode) ; Preview files in minibuffer
-  ;; (dirvish-side-follow-mode) ; similar to `treemacs-follow-mode'
-  (setq dirvish-mode-line-format
-	'(:left (sort symlink) :right (omit yank index)))
-  (setq dirvish-attributes
-	'(all-the-icons file-time file-size collapse subtree-state vc-state git-msg))
-  (setq delete-by-moving-to-trash t)
-  (setq dired-listing-switches
-	"-l --almost-all --human-readable --group-directories-first --no-group")
-  :bind ; Bind `dirvish|dirvish-side|dirvish-dwim' as you see fit
-  (("C-c f" . dirvish-fd)
-   :map dirvish-mode-map ; Dirvish inherits `dired-mode-map'
-   ("a"   . dirvish-quick-access)
-   ("f"   . dirvish-file-info-menu)
-   ("y"   . dirvish-yank-menu)
-   ("N"   . dirvish-narrow)
-   ("^"   . dirvish-history-last)
-   ("h"   . dirvish-history-jump) ; remapped `describe-mode'
-   ("s"   . dirvish-quicksort)    ; remapped `dired-sort-toggle-or-edit'
-   ("v"   . dirvish-vc-menu)      ; remapped `dired-view-file'
-   ("TAB" . dirvish-subtree-toggle)
-   ("M-f" . dirvish-history-go-forward)
-   ("M-b" . dirvish-history-go-backward)
-   ("M-l" . dirvish-ls-switches-menu)
-   ("M-m" . dirvish-mark-menu)
-   ("M-t" . dirvish-layout-toggle)
-   ("M-s" . dirvish-setup-menu)
-   ("M-e" . dirvish-emerge-menu)
-   ("M-j" . dirvish-fd-jump)))
+  (use-package dirvish
+    :init
+    (dirvish-override-dired-mode)
+    :custom
+    (dirvish-quick-access-entries ; It's a custom option, `setq' won't work
+     '(("h" "~/"                          "Home")
+       ("d" "~/Downloads/"                "Downloads")
+       ("m" "/mnt/"                       "Drives")
+       ("t" "~/.local/share/Trash/files/" "TrashCan")))
+    :config
+    ;; (dirvish-peek-mode) ; Preview files in minibuffer
+    ;; (dirvish-side-follow-mode) ; similar to `treemacs-follow-mode'
+    (setq dirvish-mode-line-format
+	  '(:left (sort symlink) :right (omit yank index)))
+    (setq dirvish-attributes
+	  '(all-the-icons file-time file-size collapse subtree-state vc-state git-msg))
+    (setq delete-by-moving-to-trash t)
+    (setq dired-listing-switches
+	  "-l --almost-all --human-readable --group-directories-first --no-group")
+    :bind ; Bind `dirvish|dirvish-side|dirvish-dwim' as you see fit
+    (("C-c f" . dirvish-fd)
+     :map dirvish-mode-map ; Dirvish inherits `dired-mode-map'
+     ("a"   . dirvish-quick-access)
+     ("f"   . dirvish-file-info-menu)
+     ("y"   . dirvish-yank-menu)
+     ("N"   . dirvish-narrow)
+     ("^"   . dirvish-history-last)
+     ("h"   . dirvish-history-jump) ; remapped `describe-mode'
+     ("s"   . dirvish-quicksort)    ; remapped `dired-sort-toggle-or-edit'
+     ("v"   . dirvish-vc-menu)      ; remapped `dired-view-file'
+     ("TAB" . dirvish-subtree-toggle)
+     ("M-f" . dirvish-history-go-forward)
+     ("M-b" . dirvish-history-go-backward)
+     ("M-l" . dirvish-ls-switches-menu)
+     ("M-m" . dirvish-mark-menu)
+     ("M-t" . dirvish-layout-toggle)
+     ("M-s" . dirvish-setup-menu)
+     ("M-e" . dirvish-emerge-menu)
+     ("M-j" . dirvish-fd-jump)))
 
 (use-package helpful
   :commands (helpful-callable helpful-variable helpful-command helpful-key)
