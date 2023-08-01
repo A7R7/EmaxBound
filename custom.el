e28f09409873b92ff21a18a7207be4dfc073185a
A7R7 <Aaron__Lee_@outlook.com>
2023-08-01 22:31:44 +0800

    show which key in posframe

---
+++ custom.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(dashboard-image-banner-max-height 800)
+ '(dashboard-image-banner-max-width 1500)
+ '(global-visual-fill-column-mode t)
+ '(global-visual-line-mode t)
+ '(package-selected-packages '(goto-chg epkg diff-hl dash auto-compile all-the-icons)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
