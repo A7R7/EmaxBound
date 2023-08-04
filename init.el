b096aeb348f93e99df6823ed7da8e6fea91b9577
A7R7 <Aaron__Lee_@outlook.com>
2023-08-05 04:28:11 +0800

    Add meow-edit and its configs

--- init.el
+++ init.el
@@ -67,7 +67,7 @@
   :config (or (server-running-p) (server-mode)))
 
 (progn ;     startup
-  (message "Loading early birds...done (%.3fs)"
+  (message "Loading early birds...done (%fs)"
            (float-time (time-subtract (current-time) before-user-init-time))))
 
 (use-package diff-mode
@@ -202,6 +202,60 @@
   :config
   (evil-set-undo-system 'undo-tree)
   (global-undo-tree-mode 1)
+)
+
+(use-package meow
+:config
+  (defun meow-setup ()
+    (setq meow-cheatsheet-layout meow-cheatsheet-layout-qwerty)
+    (meow-motion-overwrite-define-key
+     '("j" . meow-next)
+     '("k" . meow-prev)
+     '("<escape>" . ignore))
+    (meow-leader-define-key
+     ;; SPC j/k will run the original command in MOTION state.
+     '("j" . "H-j") '("k" . "H-k")
+     ;; Use SPC (0-9) for digit arguments.
+     '("1" . meow-digit-argument) '("2" . meow-digit-argument) '("3" . meow-digit-argument)
+     '("4" . meow-digit-argument) '("5" . meow-digit-argument) '("6" . meow-digit-argument)
+     '("7" . meow-digit-argument) '("8" . meow-digit-argument) '("9" . meow-digit-argument)
+     '("0" . meow-digit-argument) '("/" . meow-keypad-describe-key) '("?" . meow-cheatsheet))
+    (meow-normal-define-key
+     '("1" . meow-expand-1) '("2" . meow-expand-2) '("3" . meow-expand-3)
+     '("4" . meow-expand-4) '("5" . meow-expand-5) '("6" . meow-expand-6)
+     '("7" . meow-expand-7) '("8" . meow-expand-8) '("9" . meow-expand-9)
+     '("0" . meow-expand-0)
+     '("-" . negative-argument) '(";" . meow-reverse)
+     '("," . meow-inner-of-thing) '("." . meow-bounds-of-thing)
+     '("[" . meow-beginning-of-thing) '("]" . meow-end-of-thing)
+     '("a" . meow-append) '("A" . meow-open-below)
+     '("b" . meow-back-word) '("B" . meow-back-symbol)
+     '("c" . meow-change) '("d" . meow-delete)
+     '("D" . meow-backward-delete)
+     '("e" . meow-next-word) '("E" . meow-next-symbol)
+     '("f" . meow-find)
+     '("g" . meow-cancel-selection) '("G" . meow-grab)
+     '("h" . meow-left) '("H" . meow-left-expand)
+     '("i" . meow-insert) '("I" . meow-open-above)
+     '("j" . meow-next) '("J" . meow-next-expand)
+     '("k" . meow-prev) '("K" . meow-prev-expand)
+     '("l" . meow-right) '("L" . meow-right-expand)
+     '("m" . meow-join)
+     '("n" . meow-search)
+     '("o" . meow-block) '("O" . meow-to-block)
+     '("p" . meow-yank)
+     '("q" . meow-quit) '("Q" . meow-goto-line)
+     '("r" . meow-replace) '("R" . meow-swap-grab)
+     '("s" . meow-kill)
+     '("t" . meow-till)
+     '("u" . meow-undo) '("U" . meow-undo-in-selection)
+     '("v" . meow-visit)
+     '("w" . meow-mark-word) '("W" . meow-mark-symbol)
+     '("x" . meow-line) '("X" . meow-goto-line)
+     '("y" . meow-save) '("Y" . meow-sync-grab)
+     '("z" . meow-pop-selection)
+     '("'" . repeat) '("<escape>" . ignore)))
+      (meow-setup)
 )
 
 ;; Make ESC quit prompts
@@ -346,8 +400,8 @@
   :height 180
 )
 (set-face-attribute 'fixed-pitch nil
-  :font "Sarasa Gothic SC"
-  :height 180
+  :font "Sarasa Fixed SC"
+  :height 155
 )
 
 (set-face-attribute 'font-lock-comment-face nil
